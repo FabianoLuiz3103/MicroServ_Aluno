680fe06793ef5a45a39a27bd95b3ca44947a7586
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2024-09-24 21:40:22 -0300

    Adicionando testes web e de integração

--- src/test/java/br/com/fiap/ms_aluno/controller/AlunoControllerTestsIT.java
+++ src/test/java/br/com/fiap/ms_aluno/controller/AlunoControllerTestsIT.java
@@ -4,6 +4,7 @@ import br.com.fiap.ms_aluno.dto.DadosAtualizacaoAluno;
 import br.com.fiap.ms_aluno.dto.DadosCadastroAluno;
 import br.com.fiap.ms_aluno.dto.DadosDetalhamentoAluno;
 import br.com.fiap.ms_aluno.factory.Factory;
+import br.com.fiap.ms_aluno.model.Aluno;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -108,6 +109,76 @@ public class AlunoControllerTestsIT {
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.nome").exists())
                 .andExpect(jsonPath("$.nome").value("Fabiano Luiz"));
+    }
+
+    @Test
+    public void insertShouldThrowsExceptionWhenInvalidData() throws Exception {
+        var aluno = new Aluno();
+        aluno.setNome("Aluno example");
+        aluno.setRm("11111");
+
+        var json = objectMapper.writeValueAsString(aluno);
+
+        mockMvc.perform(post("/alunos")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    public void updateShouldUpdateAndReturnAlunoWhenIdExists() throws Exception {
+
+        var json = objectMapper.writeValueAsString(dadosAtualizacaoAluno);
+        mockMvc.perform(put("/alunos/{id}", existingId)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.nome").exists())
+                .andExpect(jsonPath("$.status").exists())
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+
+        var json = objectMapper.writeValueAsString(dadosAtualizacaoAluno);
+
+        mockMvc.perform(put("/alunos/{id}", nonExistingId)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void deleteShouldReturnNoContentWhenIdExists() throws Exception {
+
+        mockMvc.perform(delete("/alunos/{id}", existingId)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void deleteShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+
+        mockMvc.perform(delete("/alunos/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
     }
 
 
